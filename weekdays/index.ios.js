e43d2da433e62298aecf5dd7e11f07b80463d758
Alberto <alberto.commit@github.com>
2016-03-30 21:09:08 +0200

    Added some style.

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -3,17 +3,27 @@ var React = require('react-native');
 var AppRegistry = React.AppRegistry;
 var Text = React.Text;
 var View = React.View;
+var StyleSheet = React.StyleSheet;
 
 // Create a react component
 var Weekdays = React.createClass({
-  render: function() {
+  render() {
     return (
-      <View>
+      <View style={ styles.container }>
         <Text>
           Days of the week:
         </Text>
       </View>
     );
+  }
+});
+
+//Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',  // Vertical
+    alignItems: 'center'       // Horizontal
   }
 });
 
