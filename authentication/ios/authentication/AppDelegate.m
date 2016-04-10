28f49490f23517f25ed6469471f2ce477341a4c0
Alberto <alberto.commit@github.com>
2016-04-10 19:49:07 +0200

    Change module name to fix iOS version.

--- authentication/ios/authentication/AppDelegate.m
+++ authentication/ios/authentication/AppDelegate.m
@@ -44,7 +44,7 @@
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"authentication"
+                                                      moduleName:@"Authentication"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 
